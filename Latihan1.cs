abcaa4d53a92a16dfde5e64b9e9b76b613682873
Dwiky-Rama <71113373+Dwiky-Rama@users.noreply.github.com>
2020-09-27 10:29:52 +0700

    Add files via upload

---
+++ Latihan1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    //fungsi Awake
+    void Update()
+    {
+        Debug.Log("Awake Dipanggil");
+    }
+
+    //fungsi Start
+    void Start()
+    {
+        Debug.Log("Start Dipanggil");
+    }
+}
