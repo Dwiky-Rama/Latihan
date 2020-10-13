1c0cc29a6203f0bd15e211e289c9ddc6b25d787b
Dwiky-Rama <71113373+Dwiky-Rama@users.noreply.github.com>
2020-10-13 23:38:06 +0700

    Add files via upload

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] private float delayLoading = 3f;
+
+    [SerializeField] private string namaScene;
+
+    private float timeElapsed;
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayLoading)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+}
