2c64cdc349156f54a9303115483a0769661bf949
Dwiky-Rama <71113373+Dwiky-Rama@users.noreply.github.com>
2020-11-24 20:03:22 +0700

    Add files via upload

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Data
+public class Data : MonoBehaviour
 {
-    public static int score;
+    public static float coin = 300;
+    public static bool isGameOver = false;
+    public static bool isComplete = false;
 }
